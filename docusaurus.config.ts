4b9020eb12242272841e2808831c353e214d0e0e
Daniel Molnar <stanis.molnar@gmail.com>
2025-05-27 17:34:55 +0200

    Fix url

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -15,7 +15,7 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://github.com/stanis-molnar',
+  url: 'https://stanis-molnar.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/MolnarApps',
